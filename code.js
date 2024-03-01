58b7e8d91c36d5ebe015f3d0c6a3016769092faa
YuriKauaBento <161033127+YuriKauaBento@users.noreply.github.com>
2024-03-01 10:31:16 -0300

    Update code.js

--- code.js
+++ code.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
     const texto = document.body.querySelector("#texto");
-    const amogus = document.body.querySelector("#amogus");
     texto.addEventListener("click", () =>{
         if (texto.textContent === "Olá Mundo")
             texto.textContent = "Hello World";
